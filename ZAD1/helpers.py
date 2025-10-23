cbd486af2c3a3501364b21d0d9275f090a094647
ozwonek <azwonek@student.agh.edu.pl>
2025-10-23 22:56:25 +0200

    Add helpers for adding padding

---
+++ ZAD1/helpers.py
@@ -0,0 +1,24 @@
+import numpy as np
+from Float import Float
+
+def add_padding(A: np.ndarray, B: np.ndarray):
+
+    n, kA = A.shape
+    kB, m = B.shape
+
+    mx = max(n, kA, m)
+    def next_power_of_two(n):
+        return 1 << (n - 1).bit_length()
+    
+    next_power_of_two = next_power_of_two(mx)
+
+    A_pad, B_pad = np.zeros((next_power_of_two, next_power_of_two)), np.zeros((next_power_of_two, next_power_of_two))
+    A_pad[:n, :kA] = A
+    B_pad[:kB, :m] = B
+    A_pad = np.frompyfunc(Float, 1, 1)(A_pad)
+    B_pad = np.frompyfunc(Float, 1, 1)(B_pad)
+
+    return A_pad, B_pad, n, m 
+
+
+
