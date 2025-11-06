c9ad03e8fba787cfe549dc14c0de8b718b41cb63
ozwonek <azwonek@student.agh.edu.pl>
2025-11-06 23:11:53 +0100

    Fix in stats

--- ZAD2/stats.py
+++ ZAD2/stats.py
@@ -9,15 +9,15 @@ from inverse import *
 from lu import *
 
 
-def create_statistics(n: int, m:int, k: int, func, name, mul):
+def create_statistics(n: int, m:int, k: int, func, name):
     results = []
     for i in range(n,m,10):
         A = random_matrix(i)
         b = random_vector_T(i)
         for j in range(k):
                 if name == "gauss":
-                    _, stats = func(A,b, mul)
-                _, stats = func(A, mul)
+                    _, stats = func(A,b)
+                _, stats = func(A)
                 stats['size'] = i
                 stats['method'] = name
                 results.append(stats)
@@ -27,7 +27,7 @@ def create_statistics(n: int, m:int, k: int, func, name, mul):
     return avg_df
 
 
-def plot_metric(df, metric, functionName, mul):
+def plot_metric(df, metric, functionName):
     plt.figure(figsize=(8,5))
 
     for method, group in df.groupby('method'):
@@ -36,19 +36,19 @@ def plot_metric(df, metric, functionName, mul):
     plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True)) 
     plt.xlabel("Size (n)")
     plt.ylabel(metric)
-    plt.title(f"{metric} for matrix {functionName} of size n using {mul} multiplication" )
+    plt.title(f"{metric} for matrix {functionName} of size n" )
     plt.legend()
     plt.grid(True)
     plt.show()
 
 @benchmark()
-def gauss_elimination_benchmark(A: np.ndarray, b: np.ndarray, mul):
-     return gauss_elimination(A,b, mul)
+def gauss_elimination_benchmark(A: np.ndarray, b: np.ndarray):
+     return gauss_elimination(A,b)
 
 @benchmark()
-def inverse_benchmark(A: np.ndarray, mul):
-     return inverse(A, mul)
+def inverse_benchmark(A: np.ndarray):
+     return inverse(A)
 
 @benchmark()
-def lu_factorization_benchmark(A: np.ndarray, mul):
-     return lu_factorization(A, mul)
+def lu_factorization_benchmark(A: np.ndarray):
+     return lu_factorization(A)
